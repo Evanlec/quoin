e1a30cc65d3a30a08dfa81ae6206361d6a1e2281
Evan LeCompte <elecompte@1aauto.com>
2013-07-28 16:04:49 -0400

    re-write

--- models.py
+++ models.py
@@ -1,60 +1,90 @@
 from datetime import date
 
-# these should probably be in a data file or database instead of hardcoded
-ride_rates = {'bus':6.00, 'subway':4.00, 'rail':5.00, 'seniorbus':2.00}
-monthly_rates = {'bus':60.00, 'subway':40.00, 'rail':50.00, 'seniorbus':20.00}
-discounts = {'regular': 1.0, 'student':0.50, 'senior':0.50, 'employee':0.0}
-
 class Customer(object):
     def __init__(self, type, ticket):
-        if isinstance(ticket, Ticket):
-            self.ticket = ticket
-        else:
-            raise TypeError
-        if type in discounts:
-            self.type = type
-        else:
-            raise ValueError("Bad customer type value: " + type)
+        self.ticket = ticket
+        self.type = type
     def takeRide(self, ride, date):
-        if not isinstance(ride, Transport):
-            raise TypeError
         if self.ticket.type == 'balance':
             self.ticket.balance = (self.ticket.balance - (ride.fee * discounts[self.type]))
             return self.ticket.balance
-        if self.ticket.type == 'unlimited' and self.ticket.mode != ride.type:
+        elif self.ticket.type == 'unlimited' and self.ticket.mode != ride.type:
             raise ValueError("Customer cannot use this type of ticket on this ride!")
+        else:
+            raise ValueError("Bad customer type value: " + type)
 
 class Ticket(object):
     def __init__(self, type, purchase_date, mode=None, balance=0.00):
         self.type = type
         self.mode = mode
         self.purchase_date = purchase_date
-        if self.type == 'balance':
-            self.balance = balance
-        if self.type == 'unlimited':
-            self.monthly_price = self.calculateMonthlyPrice()
+        self.balance = balance
+    def adjustBalance(self, amount):
+        if (self.balance + amount) < 0:
+            print('Error: ticket balance less than zero') 
+        else:
+            self.balance = (self.balance + amount)
+
+class Ride(object):
+    def __init__(self, mode, date, ride_length=0):
+        self.mode = mode
+        self.date = date
+        self.ride_length = ride_length
+
+class VendingMachine(object):
+    """Vending Machine class contains all relevant 'actions' for the system, and is initialized with
+    rates/discount values, contains most of all business logic"""
+    def __init__(self, rates, discounts):
+        self.rates = rates
+        self.discounts = discounts
+
+    def dispensePass(self, customer, ticket, balance=0.00):
+        if ticket.type == 'balance':
+            ticket.balance = balance
+            customer.ticket = ticket
+            return "Customer purchased normal pass with starting balance of: %d" % ticket.balance
+        if ticket.type == 'unlimited':
+            base_price = self.rates[ticket.mode]['monthly']
+            day_purchased = ticket.purchase_date.day
+            if day_purchased > 15:
+                ticket.monthly_price = base_price * 0.50
+            else:
+                ticket.monthly_price = base_price
+            customer.ticket = ticket
+
+    def addMoneyToPass(self, ticket, amount):
+        if ticket.type == 'unlimited':
+            raise ValueError('Cannot add money to monthly pass type')
+        ticket.balance += amount
+        return "New ticket balance: %d" % ticket.balance
+
+    def swipePass(self, customer, ride):
+        """pass is swiped when customer takes a ride"""
+        if customer.ticket.type == 'balance':
+            customer.ticket.adjustBalance(-1 * self.getSingleRidePrice(customer, ride))
+            return customer.ticket.balance
+        if customer.ticket.type == 'unlimited':
+            if customer.ticket.mode != ride.mode:
+                return "Error: Customer cannot use this type of ticket for this ride!"
+
+    def checkBalance(self, ticket):
+        return ticket.balance
+
+    def getSingleRidePrice(self, customer, ride):
+        # customer type discount
+        fee = (self.rates[ride.mode] * self.discounts['customer'][customer.type])
+        # weekend discount
+        if ride.date.weekday() > 4:
+            fee = fee * 0.75
+        if ride.length > 0:
+            fee = fee * (1 - (self.discounts['length'] * length))
+
+        return fee
+
     def calculateMonthlyPrice(self):
-        self.monthly_price = monthly_rates[self.mode]
+        self.monthly_price = transports[self.mode]['monthly']
         day_purchased = self.purchase_date.day
         if day_purchased > 15:
             return self.monthly_price * 0.50
         else:
             return self.monthly_price
-
-
-class Transport(object):
-    def __init__(self, type):
-        if type in ride_rates:
-            self.type = type
-        else:
-            raise ValueError
-        self.time = date
-        self.fee = ride_rates[self.type]
-
-class VendingMachine(object):
-    def dispensePass(self, customer, ticket):
-        pass
-
-class SwipeMachine(object):
-    def swipePass(self, customer):
-        pass
--- tests.py
+++ tests.py
@@ -2,25 +2,45 @@ import unittest
 from datetime import date
 import models
 
-class TransportationTest(unittest.TestCase):
+rates = {
+    'bus': {'single_ride': 6.00, 'monthly': 60.00},
+    'subway': {'single_ride': 4.00, 'monthly': 40.00},
+    'rail': {'single_ride': 5.00, 'monthly': 50.00},
+    'seniorbus': {'single_ride': 2.00, 'monthly': 20.00}
+}
+discounts = {
+    'customer': {'adult': 1.0, 'student':0.50, 'senior':0.50, 'employee':0.0},
+    'length': 0.10
+}
+
+class TestMonthlyPasses(unittest.TestCase):
     def SetUp(self):
-        # not sure what to put here?
-        pass
-    def test_new_customer(self):
-        """Make sure customer cannot be initialized with bogus type value"""
-        ticket = models.Ticket(type='balance', purchase_date=date(2013,5,1), mode='bus', balance=45.00)
-        # kind of sucks that Customer cannot be tested on its own
-        with self.assertRaises(ValueError): models.Customer(type='foo', ticket=ticket)
+        self.rates = rates
+        self.discounts = discounts
+        self.ticket = models.Ticket(type='unlimited', mode='bus', purchase_date=date(2013,5,1))
+        self.cust = models.Customer(type='senior', ticket=self.ticket)
+    def test_take_ride(self):
+        ride = models.Ride('bus', date(2013, 5, 23))
+        self.machine.swipePass(self.cust, ride)
+        self.assertEqual(42.50, self.cust.ticket.balance)
+    def test_purchase_ticket(self):
+        machine.dispensePass(self.cust, self.ticket)
+        self.assertEqual(30.00, ticket.monthly_price)
+
+class TestBalancePasses(unittest.TestCase):
+    """Tests for passes with a balance loaded onto them"""
+    def SetUp(self):
+        self.rates = rates
+        self.discounts = discounts
+        machine = models.VendingMachine(self.rates, self.discounts)
+        ticket = models.Ticket(type='balance', purchase_date=date(2013,5,1), balance=45.00)
+        cust = models.Customer(type='senior', ticket=ticket)
     def test_take_ride(self):
         """Test that ticket balance is adjusted properly when taking a ride"""
-        cust = models.Customer(type='senior', ticket=models.Ticket('unlimited', 'bus', date(2013,5,1), 45.00))
-        ride = models.Transport('rail')
-        # should takeRide be a method of Customer?
-        cust.takeRide(ride, date(2013, 5, 23))
-        self.assertEqual(42.50, cust.ticket.balance)
-    def test_purchase_ticket(self):
-        ticket = models.Ticket('unlimited', 'subway', date(2013,5,25))
-        self.assertEqual(20.00, ticket.monthly_price)
+        machine = models.VendingMachine(self.rates, self.discounts)
+        ride = models.Ride('rail', date(2013, 5, 23))
+        machine.swipePass(self.cust, ride)
+        self.assertEqual(42.50, self.cust.ticket.balance)
 
 if __name__ == '__main__':
     unittest.main()
